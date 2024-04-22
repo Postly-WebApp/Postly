fc78b3a7a59e3c59f8e30d4b7b3015bc1ec08607
Zyad Elsawy <zayd202096@gmail.com>
2024-04-21 21:49:42 -0700

    making MONGO_URL global

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const dotenv = require("dotenv");
 dotenv.config();
 app.listen(5000, () => console.log(`listining in port 5000....`));
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect("mongodb+srv://Sawy:Zyad1234@cluster0.ykwk8jf.mongodb.net/");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
