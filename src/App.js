e9ea022be3d7dd884fe80ac88c3888efcc7e9578
Zyad Elsawy <zayd202096@gmail.com>
2024-04-24 10:36:30 -0700

    API link Edit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const initialValues = {
 const App = () => {
   const onSubmit = async (values, actions) => {
     try {
-      const response = await fetch("/api/auth/register", {
+      const response = await fetch("http://localhost:5000/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
