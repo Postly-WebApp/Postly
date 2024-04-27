d16a1e4638cc9a9119dd2891ef08bf791faa9fa1
Zyad Elsawy <zayd202096@gmail.com>
2024-04-27 11:26:44 -0700

    API Integration Issue Fixed (sign up)

--- src/App.js
+++ src/App.js
@@ -1,65 +1,85 @@
 import React from "react";
-import './App.css';
 import { Formik, Form, Field } from "formik";
+import "./App.css";
+import { signupSchema } from "./schemas";
 import { TextField, Grid } from "@mui/material";
-import emailIcon from './png/email.png';
-import passwordIcon from './png/password.png';
-
+import userIcon from "./Assets/person.png";
+import emailIcon from "./Assets/email.png";
+import passwordIcon from "./Assets/password.png";
+import axios from "axios";
 
 const initialValues = {
+  username: "",
   email: "",
   password: "",
 };
 
 const App = () => {
-  const onLogin = async (values, actions) => {
+  const onSubmit = async (values, actions) => {
+    console.log("Submitting form with values:", values);
+    const { username, email, password } = values;
+    const userData = { username, email, password };
     try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(values),
-      });
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        userData
+      );
 
-      if (response.ok) {
-        console.log('Login successful');
+      if (response.status === 200 || response.status === 201) {
+        console.log("Sign-up successful");
         window.location.href = "/home";
       } else {
-        const errorData = await response.json();
-        console.error('Login error:', errorData);
+        console.error("Sign-up error:", response.data);
       }
     } catch (error) {
-      console.error('Login request failed:', error);
+      console.error("Sign-up request failed:", error.response.data);
     } finally {
       actions.resetForm();
     }
   };
 
-  const handleSignUpClick = () => {
-    window.location.href = "/signup";
-  };
-
-  const handleCancel = (actions) => {
-    actions.resetForm();
+  const handleCancel = (resetForm) => {
+    resetForm();
   };
 
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Login</div>
+        <div className="text">Sign Up</div>
         <div className="underline"></div>
       </div>
       <Formik
         initialValues={initialValues}
-        onSubmit={onLogin}
+        validationSchema={signupSchema}
+        onSubmit={onSubmit}
       >
-        {({ errors, touched }) => (
+        {({ values, errors, touched, resetForm }) => (
           <Form className="inputs">
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <div className="input">
-                  <img src={emailIcon} alt="Email icon" className="input-icon" />
+                  <img src={userIcon} alt="User icon" className="input-icon" />
+                  <Field
+                    as={TextField}
+                    name="username"
+                    className="input-field"
+                    placeholder="Name"
+                    error={!!errors.username && touched.username}
+                    helperText={
+                      errors.username && touched.username ? errors.username : ""
+                    }
+                    fullWidth
+                  />
+                </div>
+              </Grid>
+
+              <Grid item xs={12}>
+                <div className="input">
+                  <img
+                    src={emailIcon}
+                    alt="Email icon"
+                    className="input-icon"
+                  />
                   <Field
                     as={TextField}
                     type="email"
@@ -67,14 +87,21 @@ const App = () => {
                     className="input-field"
                     placeholder="Email"
                     error={!!errors.email && touched.email}
-                    helperText={errors.email && touched.email ? errors.email : ""}
+                    helperText={
+                      errors.email && touched.email ? errors.email : ""
+                    }
                     fullWidth
                   />
                 </div>
               </Grid>
+
               <Grid item xs={12}>
                 <div className="input">
-                  <img src={passwordIcon} alt="Password icon" className="input-icon" />
+                  <img
+                    src={passwordIcon}
+                    alt="Password icon"
+                    className="input-icon"
+                  />
                   <Field
                     as={TextField}
                     type="password"
@@ -82,13 +109,25 @@ const App = () => {
                     className="input-field"
                     placeholder="Password"
                     error={!!errors.password && touched.password}
-                    helperText={errors.password && touched.password ? errors.password : ""}
+                    helperText={
+                      errors.password && touched.password ? errors.password : ""
+                    }
                     fullWidth
                   />
                 </div>
               </Grid>
+
               <div className="submit-container">
-                <button type="submit" className="submit">Login</button>
+                <button type="submit" className="submit">
+                  Sign Up
+                </button>
+                <button
+                  type="button"
+                  className="submit"
+                  onClick={() => handleCancel(resetForm)}
+                >
+                  Cancel
+                </button>
               </div>
             </Grid>
           </Form>
