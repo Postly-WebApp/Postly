a4b64ac2c10c897c5a8edc4c3aad0614072b36f7
Zyad Elsawy <zayd202096@gmail.com>
2024-05-05 22:31:26 +0300

    Added Comments Endpoints

--- routes/comments.js
+++ routes/comments.js
@@ -4,11 +4,12 @@ const jwt = require("jsonwebtoken");
 // route to add comment to a specific post by a specific user
 //const { validateComment, sanitizeComment } = require("./utils/validation");
 
+// create a new comment
 router.post("/", async (req, res) => {
   try {
     const token = req.cookies.jwt;
     if (!token) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: "user not authenticated" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -21,18 +22,27 @@ router.post("/", async (req, res) => {
     // }
 
     // Check if the userID matches the decoded token
-    if (req.body.author !== userID) {
-      console.log(userID);
-      console.log(req.body.userID);
-      return res.status(403).json({ error: "Unauthorized access" });
-    }
+    // if (req.body.author !== userID) {
+    //   console.log(userID);
+    //   console.log(req.body.author);
+    //   return res.status(403).json({
+    //     error: "you only can add comments for you not someone else :)",
+    //   });
+    // }
 
-    const newComment = new Comment(req.body);
+    const newComment = new Comment({
+      ...req.body,
+      author: userID,
+    });
     const savedComment = await newComment.save();
-    res.status(200).json(savedComment);
+    res.status(200).json({
+      success: "True",
+      message: "Comment added successfully",
+      commentID: savedComment._id,
+    });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ error: "something went wrong" });
   }
 });
 
@@ -40,27 +50,41 @@ router.post("/", async (req, res) => {
 router.get("/:postID", async (req, res) => {
   try {
     const comments = await Comment.find({ postID: req.params.postID });
+    if (comments.length === 0) {
+      return res.status(404).json({ message: "No comments found" });
+    }
     res.status(200).json(comments);
   } catch (err) {
-    res.status(500).json(err);
+    console.log(err.message);
+    res.status(500).json({ error: "something went wrong" });
   }
 });
 
-// route to get all comments in the db
-router.get("/", async (req, res) => {
-  try {
-    const comments = await Comment.find();
-    res.status(200).json(comments);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// route to get all comments for a specific post
+// router.get("/", async (req, res) => {
+//   try {
+//     const comments = await Comment.find();
+//     res.status(200).json(comments);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // route to delete a specific comment with checking the author
 router.delete("/:id", async (req, res) => {
   try {
+    const token = req.cookies.jwt;
+    console.log(token);
+    if (!token) {
+      return res.status(401).json({ error: "Unauthenticated" });
+    }
     const comment = await Comment.findById(req.params.id);
-    if (comment.author === req.body.userID) {
+    if (!comment) {
+      return res.status(404).json({ message: "comment does not exist" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userID = decoded.id;
+    if (comment.author.toString() === userID) {
       await comment.deleteOne();
       res
         .status(200)
@@ -68,11 +92,65 @@ router.delete("/:id", async (req, res) => {
     } else {
       res.status(403).json({
         success: "false",
-        message: "you can only delete your own comments",
+        message: "you can only delete your own comments :)",
       });
     }
   } catch (err) {
-    res.status(500).json(err);
+    console.log(err.message);
+    res.status(500).json({ error: "something went wrong" });
+  }
+});
+
+// route to update a specific comment
+router.put("/:id", async (req, res) => {
+  try {
+    const token = req.cookies.jwt;
+    console.log(token);
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const comment = await Comment.findById(req.params.id);
+    if (!comment) {
+      return res.status(404).json({ message: "comment does not exist" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userID = decoded.id;
+    if (comment.author.toString() !== userID) {
+      return res.status(403).json({
+        success: "false",
+        message: "you can only update your own comments :)",
+      });
+    }
+    const updatedComment = await Comment.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: { text: req.body.text },
+      },
+      { new: true }
+    );
+    res.status(200).json({
+      success: "true",
+      message: "Updated successfully",
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: "something went wrong" });
   }
 });
 module.exports = router;
+
+/*
+  -------------------------------------      Reviewed and Done (FOR NOW)       -------------------------------------
+  # DOCs
+    -> create a new comment
+        => reviewd
+        => DONE
+    -> get all comments for a specific post
+        => reviewd
+        => DONE
+    -> delete a specific comment
+        => reviewd
+        => DONE
+    -> update a specific comment
+        => reviewd
+*/
